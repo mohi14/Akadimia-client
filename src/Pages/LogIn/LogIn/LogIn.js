2a3dfe052c314d553af5dbcee52bb28c6172b4e2
mohi14 <mohiuddinkhan1409@gmail.com>
2022-10-25 17:01:51 +0600

    login page created

--- src/Pages/LogIn/LogIn/LogIn.js
+++ src/Pages/LogIn/LogIn/LogIn.js
@@ -1,8 +1,44 @@
 import React from 'react';
-
+import { Link } from 'react-router-dom';
+import { CgGoogle } from "react-icons/cg";
+import { IoLogoGithub } from "react-icons/io5";
 const LogIn = () => {
     return (
-        <div></div>
+        <div className="hero min-h-screen bg-base-200">
+            <div className="hero-content flex-col">
+                <div className="text-center lg:text-left mb-6">
+                    <h1 className="text-5xl font-bold">Please Login now!</h1>
+                </div>
+                <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+                    <form className="card-body">
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text font-semibold">Email</span>
+                            </label>
+                            <input type="text" placeholder="Enter your email" className="input input-bordered input-primary w-full max-w-xs" />
+                        </div>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text font-semibold">Password</span>
+                            </label>
+                            <input type="text" placeholder="Enter your password" className="input input-bordered input-primary w-full max-w-xs" />
+                            <label className="label font-semibold">
+                                <Link href="#" className="label-text-alt link link-hover">Forgot password?</Link>
+                            </label>
+                        </div>
+                        <div className="form-control mt-3">
+                            <button className="btn btn-primary">Login</button>
+                            <div className="flex flex-col w-full border-opacity-50">
+                                <div className="divider">OR</div>
+                                <div className=" btn  bg-base-300  place-items-center mb-4"><CgGoogle className='mr-2 text-xl' />Google</div>
+                                <div className=" btn  bg-base-300  place-items-center"><IoLogoGithub className='mr-2 text-xl' /> Github</div>
+                            </div>
+                        </div>
+
+                    </form>
+                </div>
+            </div>
+        </div>
     );
 };
 
