bc7ae2192001a727ac5ef8e65dec424d80792be5
mohi14 <mohiuddinkhan1409@gmail.com>
2022-10-26 14:15:01 +0600

    banner section done

--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -1,8 +1,22 @@
 import React from 'react';
+import './Home.css'
 
 const Home = () => {
     return (
-        <div>this is home</div>
+        <>
+            <div className='flex p-10 lg:px-40 lg:mb-60'>
+                <div className=' w-full lg:w-3/5  lg:pt-32'>
+                    <h1 className='text-4xl lg:text-6xl font-semibold '>Learn without limits</h1>
+                    <p className='lg:text-xl mt-7 mb-6'>Start, switch, or advance your career with more than 5,200 courses, Professional Certificates, and degrees from world-class universities and companies.</p>
+                    <button className='w-full lg:w-40 btn btn-primary lg:mr-3 mb-5'>Join for Free</button> <button className='w-full lg:w-60 btn btn-outline btn-primary'>Try Akadimia for Business</button>
+                </div>
+                <div className='w-2/5 hidden lg:flex mt-20'>
+                    <div className='bg-primary rounded-2xl absolute ml-28 shadow-2xl'>
+                        <img src="https://images.unsplash.com/photo-1532708059644-5590ed51ce4c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=388&q=80" alt="" className='h-96 rounded-2xl  relative -top-3 -left-3' />
+                    </div>
+                </div>
+            </div >
+        </>
     );
 };
 
--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -29,56 +29,34 @@ const Header = () => {
     }
 
     return (
-        <div>
-            <div className="navbar bg-neutral text-neutral-content py-3 lg:px-5">
-                <div className="navbar-start">
-                    <div className="dropdown">
-                        <label tabIndex={0} className="btn btn-ghost lg:hidden">
-                            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
-                        </label>
-                        <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
-                            <li>{user?.uid ?
-                                <>
 
-                                    {
-                                        user?.photoURL ?
-                                            <div className="avatar online mb-3 flex lg:hidden">
-                                                <div className=" w-10 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2" >
-                                                    <img className="tooltip tooltip-bottom" data-tip="hello" src={user?.photoURL} alt='' />
-                                                </div>
+        <div className="navbar bg-neutral text-neutral-content py-3 lg:px-5">
+            <div className="navbar-start">
+                <div className="dropdown">
+                    <label tabIndex={0} className="btn btn-ghost lg:hidden">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+                    </label>
+                    <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+                        <li>{user?.uid ?
+                            <>
+
+                                {
+                                    user?.photoURL ?
+                                        <div className="avatar  mb-3 flex lg:hidden">
+                                            <div className=" w-10 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2" >
+                                                <img className="tooltip tooltip-bottom" data-tip="hello" src={user?.photoURL} alt='' />
                                             </div>
-                                            :
-                                            <div class=" flex lg:hidden overflow-hidden relative w-10 h-10 bg-gray-100 rounded-full dark:bg-gray-600 mr-5 online avatar mb-3">
-                                                <svg class="absolute -left-1 w-12 h-12 text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z" clip-rule="evenodd"></path></svg>
-                                            </div>
-                                    }
-                                    <button className='btn btn-outline btn-primary flex lg:hidden' >LogOut</button>
-                                </>
-                                :
-                                <Link to='/login' className='mr-2 btn btn-outline btn-primary  lg:hidden flex'>Login</Link>
-                            }</li>
-
-                            <li><NavLink to='/home' className={({ isActive }) =>
-                                isActive ? activeClassName : undefined
-                            }>Home</NavLink></li>
-
-                            <li><NavLink to='/courses' className={({ isActive }) =>
-                                isActive ? activeClassName : undefined
-                            }>Courses</NavLink></li>
-
-                            <li><NavLink to='/blog' className={({ isActive }) =>
-                                isActive ? activeClassName : undefined
-                            }>Blog</NavLink></li>
-                            <li><NavLink to='/faq' className={({ isActive }) =>
-                                isActive ? activeClassName : undefined
-                            }>FAQ</NavLink></li>
-                        </ul>
-                    </div>
-                    <Link to='/' className="btn btn-ghost normal-case font-bold lg:text-4xl text-2xl flex content-center ">
-                        <img src={Logo} alt="" className='w-12 mt-4 lg:mx-3' /> <div>Akadimia</div></Link>
-                </div>
-                <div className="navbar-center hidden lg:flex font-semibold">
-                    <ul className="menu menu-horizontal p-0">
+                                        </div>
+                                        :
+                                        <div class=" flex lg:hidden overflow-hidden relative w-10 h-10 bg-gray-100 rounded-full dark:bg-gray-600 mr-5 online avatar mb-3">
+                                            <svg class="absolute -left-1 w-12 h-12 text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z" clip-rule="evenodd"></path></svg>
+                                        </div>
+                                }
+                                <button className='btn btn-outline btn-primary flex lg:hidden' >LogOut</button>
+                            </>
+                            :
+                            <Link to='/login' className='mb-2 btn btn-outline btn-primary  lg:hidden flex'>Login</Link>
+                        }</li>
 
                         <li><NavLink to='/home' className={({ isActive }) =>
                             isActive ? activeClassName : undefined
@@ -91,46 +69,68 @@ const Header = () => {
                         <li><NavLink to='/blog' className={({ isActive }) =>
                             isActive ? activeClassName : undefined
                         }>Blog</NavLink></li>
-                        <li ><NavLink to='/faq' className={({ isActive }) =>
+                        <li><NavLink to='/faq' className={({ isActive }) =>
                             isActive ? activeClassName : undefined
                         }>FAQ</NavLink></li>
-
                     </ul>
                 </div>
-                <div className="navbar-end font-semibold">
-                    <div className='mr-2'>
-                        {status ? <HiMoon className='text-xl' /> : < HiOutlineSun className='text-xl' />}
-                    </div>
-                    <input type="checkbox" className="toggle toggle-primary" checked={status} onClick={handleDarkMode} />
+                <Link to='/' className="btn btn-ghost normal-case font-bold lg:text-4xl text-2xl flex content-center ">
+                    <img src={Logo} alt="" className='w-12 mt-4 lg:mx-3' /> <div>Akadimia</div></Link>
+            </div>
+            <div className="navbar-center hidden lg:flex font-semibold">
+                <ul className="menu menu-horizontal p-0">
 
+                    <li><NavLink to='/home' className={({ isActive }) =>
+                        isActive ? activeClassName : undefined
+                    }>Home</NavLink></li>
 
+                    <li><NavLink to='/courses' className={({ isActive }) =>
+                        isActive ? activeClassName : undefined
+                    }>Courses</NavLink></li>
 
+                    <li><NavLink to='/blog' className={({ isActive }) =>
+                        isActive ? activeClassName : undefined
+                    }>Blog</NavLink></li>
+                    <li ><NavLink to='/faq' className={({ isActive }) =>
+                        isActive ? activeClassName : undefined
+                    }>FAQ</NavLink></li>
 
-                    <div className='ml-5'>
-                        {user?.uid ?
-                            <div className='flex justify justify-center items-center tooltip  tooltip-bottom' data-tip={user?.displayName}>
-                                {
-                                    user?.photoURL ?
-                                        <div className="avatar online mr-5 hidden lg:flex">
-                                            <div className=" w-10 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2" >
-                                                <img className="tooltip tooltip-bottom" data-tip="hello" src={user?.photoURL} alt='' />
-                                            </div>
-                                        </div>
-                                        :
-                                        <div class=" overflow-hidden relative w-10 h-10 bg-gray-100 rounded-full dark:bg-gray-600 mr-5 online hidden lg:flex">
-                                            <svg class="absolute -left-1 w-12 h-12 text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z" clip-rule="evenodd"></path></svg>
-                                        </div>
-                                }
-
-                                <button className='btn btn-outline btn-primary hidden lg:flex' onClick={handleLogOut} >LogOut</button>
-                            </div>
-                            :
-                            <Link to='/login' className='mr-2 btn btn-outline btn-primary hidden lg:flex'>Login</Link>
-                        }
-                    </div>
+                </ul>
+            </div>
+            <div className="navbar-end font-semibold">
+                <div className='mr-2'>
+                    {status ? <HiMoon className='text-xl' /> : < HiOutlineSun className='text-xl' />}
                 </div>
-            </div >
+                <input type="checkbox" className="toggle toggle-primary" checked={status} onClick={handleDarkMode} />
+
+
+
+
+                <div className='ml-5'>
+                    {user?.uid ?
+                        <div className='flex justify justify-center items-center tooltip  tooltip-bottom' data-tip={user?.displayName}>
+                            {
+                                user?.photoURL ?
+                                    <div className="avatar online mr-5 hidden lg:flex">
+                                        <div className=" w-10 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2" >
+                                            <img className="tooltip tooltip-bottom" data-tip="hello" src={user?.photoURL} alt='' />
+                                        </div>
+                                    </div>
+                                    :
+                                    <div class=" overflow-hidden relative w-10 h-10 bg-gray-100 rounded-full dark:bg-gray-600 mr-5 online hidden lg:flex">
+                                        <svg class="absolute -left-1 w-12 h-12 text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z" clip-rule="evenodd"></path></svg>
+                                    </div>
+                            }
+
+                            <button className='btn btn-outline btn-primary hidden lg:flex' onClick={handleLogOut} >LogOut</button>
+                        </div>
+                        :
+                        <Link to='/login' className='mr-2 btn btn-outline btn-primary hidden lg:flex'>Login</Link>
+                    }
+                </div>
+            </div>
         </div >
+
     );
 };
 
