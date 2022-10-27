bfe0ea2b0004c4edac4e9e276aebfa597a1d838d
mohi14 <mohiuddinkhan1409@gmail.com>
2022-10-27 13:47:29 +0600

    checkout page added and horizontall scrolling fixes

--- src/Pages/CheckOut/CheckOut.js
+++ src/Pages/CheckOut/CheckOut.js
@@ -1,13 +1,35 @@
 import React from 'react';
-import { useLoaderData } from 'react-router-dom';
+import { useContext } from 'react';
+import { Link, useLoaderData } from 'react-router-dom';
+import { AuthContext } from '../../contexts/AuthProvider/AuthProvider';
 
 const CheckOut = () => {
     const course = useLoaderData();
-    console.log(course)
+    const { title, image_url, level } = course;
+
+    const { user } = useContext(AuthContext);
+
     return (
         <div>
-            <h1>{course.title}</h1>
-        </div>
+
+            <div className="flex  justify-center my-20 ">
+
+                <div className="bg-neutral text-neutral-content flex flex-col items-center pb-10 border-2 border-primary w-full lg:w-1/2 rounded-lg">
+                    <img className="my-5 w-40 h-40 rounded-full shadow-lg" src={image_url} alt="" />
+                    <h5 className="mb-1 text-3xl font-semibold">{title}</h5>
+                    <span className="text-md mb-5">{level} level</span>
+                    <h1 className='text-2xl font-semibold'>Name</h1>
+                    <p className='text-lg mb-3'>{user?.displayName}</p>
+                    <h2 className='text-2xl font-semibold'>Email</h2>
+                    <p className='text-lg'>{user?.email}</p>
+                    <div className="flex mt-4 space-x-3 md:mt-6">
+                        <Link className="btn btn-primary">Buy now</Link>
+                        <Link to='/courses' className="btn btn-primary btn-outline">View all courses</Link>
+                    </div>
+                </div>
+            </div>
+
+        </div >
     );
 };
 
--- src/Pages/CourseDetails/CourseDetail.js
+++ src/Pages/CourseDetails/CourseDetail.js
@@ -9,12 +9,12 @@ const CourseDetail = () => {
     const { _id, duration, title, total_reviews, details, starting_date, image_url, prerequisites, } = selectedCourse;
 
     return (
-        <div className='flex m-5 rounded-lg shadow-xl'>
-            <div className='w-2/5'>
+        <div className='flex flex-col md:flex-row m-2 lg:m-5 rounded-lg shadow-lg bg-neutral text-neutral-content'>
+            <div className='w-full lg:w-2/5'>
                 <img src={image_url} alt="" className='w-full rounded-l-lg  h-full' />
             </div>
-            <div className='px-20 py-5'>
-                <h1 className='text-5xl font-semibold mb-10'>{title} <button><CgSoftwareDownload className='inline text-primary' /></button></h1>
+            <div className='w-full px-20 py-5'>
+                <h1 className='text-3xl lg:text-5xl font-semibold mb-10'>{title} <button><CgSoftwareDownload className='inline text-primary' /></button></h1>
                 <p className='mb-4'>{details}</p>
                 <p className='font-semibold text-xl'>Estimate time</p>
                 <p className='mb-2'>{duration}</p>
@@ -23,7 +23,7 @@ const CourseDetail = () => {
                 <p className='mb-2'>{starting_date}</p>
                 <p className='font-semibold text-xl'>Prerequisites</p>
                 <p className='mb-7'>{prerequisites}</p>
-                <Link to={`/checkout/${_id}`}><button className='btn btn-primary'><FaCrown className='text-warning mr-3 text-xl' /> Get premium access</button></Link>
+                <Link to={`/checkout/${_id}`}><button className='btn btn-primary pb-3 lg:pb-0'><FaCrown className='text-warning mr-3 text-xl' /> Get premium access</button></Link>
             </div>
         </div >
     );
